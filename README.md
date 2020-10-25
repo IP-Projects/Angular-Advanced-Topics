2f8fb4350f161692fe4a1674380ea90db9364979
Iulian Preda <iulian.octavian.preda@gmail.com>
2020-10-25 19:17:05 +0200

    Added fontawesome

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Angular-Advanced-Topics
- Angular app meant as an exercise for advanced topics
+
+Angular app meant as an exercise for advanced topics
+
+# To Create
+
+-   npm install ngx-bootstrap bootstrap
+-   ng add @fortawesome/angular-fontawesome then add [icon library](https://github.com/FortAwesome/angular-fontawesome/blob/HEAD/docs/usage/icon-library.md#using-the-icon-library) in app.module or lazy loaded module
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,18 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { far } from '@fortawesome/free-regular-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, FontAwesomeModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {
+  constructor(library: FaIconLibrary) {
+    // Add an icon to the library for convenient access in other components
+    // library.addIcons(faCoffee);
+    library.addIconPacks(fas, far);
+  }
+}
